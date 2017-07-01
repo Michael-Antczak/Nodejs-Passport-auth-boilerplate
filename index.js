2e647ea31de288dfa3af587305e3c52e2e879ce3
Michael Antczak <Michael.Antczak@gmail.com>
2017-07-01 15:42:01 +0100

    Setup router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router')
 
 
 // DB setup
@@ -12,6 +13,7 @@ const app = express();
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.port || 3090;

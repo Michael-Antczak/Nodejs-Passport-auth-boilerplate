aaf409b1791d480ff60cecd0fed5e332bb3a0a90
Michael Antczak <Michael.Antczak@gmail.com>
2017-07-01 17:44:07 +0100

    Salt passwords

--- models/users.js
+++ models/users.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define model
 const userSchema = new Schema({
   email: { type: String, unique: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+// Before saving a model , run this function
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      // override plain text password with salted password
+      user.password = hash;
+      next();
+    });
+
+  });
 });
 
 // Create model class

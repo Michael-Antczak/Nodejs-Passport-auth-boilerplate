5d7b66d4e4f6f97abfc1285bd5af6a841059de61
Michael Antczak <Michael.Antczak@gmail.com>
2017-07-01 16:31:55 +0100

    Setup controllers

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    res.send(["waterbottle", "bottle", "pilot"])
-  });
+  app.post('/signup', Authentication.signup );
 }

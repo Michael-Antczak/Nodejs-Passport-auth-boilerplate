bac47a38c7f15291189ac79ea56d58cfa4941d85
Michael Antczak <Michael.Antczak@gmail.com>
2017-07-01 17:03:23 +0100

    Setup user signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,36 @@
+const User = require('../models/users');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' })
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if user already exists
+  User.findOne({ email: email }, function(err, existingUser) {
+
+    // Error
+    if (err) { return next(err); }
+
+    // If the user exists, return Error
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is in use."})
+    }
+
+    // If the user does not exist,
+    // create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json({
+        success: "true"
+      });
+    });
+
+  });
 }
